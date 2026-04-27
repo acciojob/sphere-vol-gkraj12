0563ccedfd1686cf9763801ad3c41f8f48b10970
gaurav kumar <gauravkumarskg851215@gmail.com>
2026-04-28 01:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  const radius = parseFloat(document.getElementById("radius").value);
+  const volumeField = document.getElementById("volume");
+  let volume = NaN;
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  if (!isNaN(radius) && radius >= 0) {
+    volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volumeField.value = volume.toFixed(4);
+  } else {
+    volumeField.value = "NaN";
+  }
+
+  return false; // form reload rokne ke liye
+}
+
+window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
